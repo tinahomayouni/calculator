9fcbb086cb1e352cb8f8217005530eaacfe5316c
Tina <tinahomayouni@gmail.com>
2020-04-01 20:30:37 +0430

    final edition

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,7 +3,7 @@ function insert(num) {
     document.querySelector(".result").value + num;
 }
 function equal() {
-  var exe = document.querySelector(".result").value;
+  let exe = document.querySelector(".result").value;
   if (exe) {
     document.querySelector(".result").value = eval(exe);
   }
